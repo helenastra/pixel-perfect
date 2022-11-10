f63589f37a51e676ce1310bc0d34090e6bae93dc
Helen <helenyu9243@gmail.com>
2022-11-09 20:55:27 -0500

    Implemented Cart, Navbar and added functionality to modify elements in Cart.

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
 
 import { Layout } from '../components';
 import '../styles/globals.css';
+import { StateContext } from '../context/StateContext'
 
 function MyApp({ Component, pageProps }) {
   return ( 
     //refers to the component we are currently on (home, details, checkout)
     //get access to this component in Layout by passing "children" 
-    // props and rendering them where we want it
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    //props and rendering them where we want it
+    //pass data  from StateContext to every component inside of it
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   )
 }
 
